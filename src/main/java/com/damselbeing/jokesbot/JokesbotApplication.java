72da5163f0d63f00145a2fbb92fc724e7bf5bdd3
damselbeing <damselbeing@gmail.com>
2023-01-05 18:23:00 +0300

    Project init.

---
+++ src/main/java/com/damselbeing/jokesbot/JokesbotApplication.java
@@ -0,0 +1,13 @@
+package com.damselbeing.jokesbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damselbeing/jokesbot/JokesbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damselbeing.jokesbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
